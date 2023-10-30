6711e86daf87e814da01a47c73685a44416dd92a
ProgSamuel <progsamu@gmail.com>
2023-10-30 10:02:03 -0300

    update style

--- src/assets/components/cardBook.tsx
+++ src/assets/components/cardBook.tsx
@@ -5,6 +5,20 @@ export const CardBook = styled.div`
   flex-direction: row;
   align-items: center;
   gap: 1em;
+  background-color: rgba(224, 224, 224, 0.7) !important;
+  position: relative;
+  backdrop-filter: blur(109px);
+
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -9;
+  }
 
   li {
     list-style: none;
@@ -29,7 +43,7 @@ export const CardBook = styled.div`
     padding: 0.5em 1.5em;
     border-radius: 5px;
     gap: 5px;
-
     background-color: #a9a9a9;
+    
   }
 `;
--- src/assets/components/form.tsx
+++ src/assets/components/form.tsx
@@ -1,30 +1,44 @@
 import styled from 'styled-components';
+
 export const FormStyled = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 3em;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    backdrop-filter: blur(10px); 
+    z-index: -1;
+  }
 
   form {
-    background-color: #e0e0e0;
+    background-color: rgba(224, 224, 224, 0.7); 
     width: 50%;
     height: fit-content;
     padding: 3em 1.5em;
     border-radius: 20px;
     display: flex;
     flex-direction: column;
-    gap: 1em;
+    gap: .6em;
 
     label {
-      font-weight: 600;
+      font-weight: 500;
     }
+
     ::placeholder {
       color: #ff6f61;
     }
 
     input,
     select {
-      background-color: #a9a9a9;
+      background-color: white;
       padding: 0.5em 1em;
       border: none;
       border-radius: 3px;
@@ -32,7 +46,7 @@ export const FormStyled = styled.div`
   }
 
   div {
-    background-color: #e0e0e0;
+    background-color: rgba(224, 224, 224, 0.7);  
     width: fit-content;
     padding: 0.5em 1.5em;
     border-radius: 20px;
